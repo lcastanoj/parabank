8cbdea26464f113458f5e3c2a8d98ab2ab4b5a07
Lucas  Castano Jaramillo <lucas.castano@sqasa.co>
2025-12-01 18:13:25 -0500

    internal transfer funcional

--- src/main/java/com/automation/utils/DriverFactory.java
+++ src/main/java/com/automation/utils/DriverFactory.java
@@ -6,19 +6,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-
     public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().clearDriverCache().setup();
+        WebDriverManager.chromedriver().setup();
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--headless=new"); // necesario en CI
+        chromeOptions.addArguments("--no-sandbox"); // evita problemas con permisos
+        chromeOptions.addArguments("--disable-dev-shm-usage"); // memoria compartida limitada
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-infobars");
-
+        chromeOptions.addArguments("--window-size=1920,1080"); // porque headless no tiene pantalla
         chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         chromeOptions.setExperimentalOption("useAutomationExtension", false);
-
-        //chromeOptions.addArguments("--remote-allow-origins=*");
 
         return new ChromeDriver(chromeOptions);
     }
